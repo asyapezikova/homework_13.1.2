5ca8b38790535dcb163d23bf12b7ddb8afe07f71
Анастасия <bobina_asya@mail.ru>
2024-09-11 14:14:49 +0300

    Added attributes and methods to the class Channel. File created "moscowpython.json".

--- src/channel.py
+++ src/channel.py
@@ -14,7 +14,31 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube/channels/{self.__channel_id}"
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('API_KEY')
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, json_file):
+        data_json = {
+            'channel_id': self.__channel_id,
+            'title': self.title,
+            'descrition': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'videoCount': self.video_count,
+            'viewCount': self.view_count
+        }
+
+        with open(json_file, 'w', encoding="utf-8") as f:
+            f.write(json.dumps(data_json))
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
